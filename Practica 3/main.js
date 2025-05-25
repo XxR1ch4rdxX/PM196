bd25b326d3931ec03aaae90ef1ca983893ef2715
XxR1ch4rdxX <167234180+XxR1ch4rdxX@users.noreply.github.com>
2025-05-25 15:48:14 -0600

    P3 Importar y Exportar
    exportar funciones y variables a otro modulo de js

---
+++ Practica 3/main.js
@@ -0,0 +1,6 @@
+const { restar,cuadrado,PI,sumar } = require('./utils');
+console.log("5 - 3 =", restar(5, 3));
+console.log("5 + 3 =", sumar(5, 3));
+console.log("El cuadrado de 5 es", cuadrado(5));
+console.log("El valor de PI es", PI);
+
---
+++ Practica 3/utils.js
@@ -0,0 +1,16 @@
+function restar(a, b) {
+    return a - b;
+}
+
+
+function sumar(a, b) {
+    return a + b;
+}
+
+function cuadrado(a){
+    return a * a;
+}
+
+PI = 3.1416;
+
+module.exports = { restar , sumar, cuadrado, PI };
