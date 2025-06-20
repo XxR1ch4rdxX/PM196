dcb4f5423faaa86fd615d1f53c36c73895849513
XxR1ch4rdxX <167234180+XxR1ch4rdxX@users.noreply.github.com>
2025-06-20 15:26:39 -0600

    Practica 7.1 Botones ->Domi
    Se crearon botones con estilos y views

--- app_sin_nada/App.js
+++ app_sin_nada/App.js
@@ -1,79 +1,96 @@
 //Zona de importaciones
 import { StatusBar } from 'expo-status-bar';    
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button ,Alert, Platform} from 'react-native';
 import React,{useState} from 'react';
 
-//Funcion main que ejecutara la vista principal de la aplicacion
-//Solo se heredan las propiedades no los estados
-//aca cambiamos el parametro de promp a style para los estilos
- const Texto=({style}) => {
-  const [contenido,setContenido] = useState('Hola');
-  const actualizarText=()=>{setContenido('Adios');}
-  //ahora el hijo de texto recibe el estilo
-  // y lo aplica al texto y no depende del padre
-  //si no le pasamos el estilo, se aplica el estilo por defecto
-     return (<Text  style={[styles.text,style]} onPress={actualizarText}> {contenido} </Text>) 
-    }
+const showAlert = (message) =>{
+if(Platform.OS === 'web'){
+  window.alert(message);
+}else{
+    Alert.alert('Alerta!!!',message)
+  }
+}
 
- const Boton=(props) => {
-  const [tituloBoton, setTituloBoton] = useState('Boton Tonto');
-  const Presionar = () => {setTituloBoton('Presionado');};
-  return (<Button onPress={Presionar} title={tituloBoton}/>)
- }
 
 export default function App() {
-
-  
-
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>React Native Button Test</Text>
 
-      
-  
-      {/* Si queremos hacer algo visible debe estar dentro de view, como si fuera el body 
-      Todo lo que queramos mostrar debe ir dentro de view y cada elemento tiene sus
-      etiquetas
-      */}
-      
+
+      <View style={styles.section}>
+        <Text style={styles.decription}>Boton Basico</Text>
+        <Button 
+          title='Presionamee'
+          onPress={()=> showAlert('Boton Apachurrao')}
+          />     
       <StatusBar style="auto" />
-      {/* Este estilo no va a funcionar ya que debe estar
-      declarado el estulilo en el padre, es decir en el componente "Text" */}
-      <Texto style={styles.text}> </Texto>
+      </View>
+      <View style={styles.section}>
+        <Text style={styles.description}>Boton con color</Text>
+        <Button
+          title='Boton con color'
+          color={'#800080'} 
+          onPress={()=> showAlert('Boton Morado Puchado')}
+        />
+      </View>
+      <View style={styles.section}>
+        <Text style={styles.description}>Boton deshabilitado:</Text>
+        <Button
+          title='Boton con color'
+          disabled={true}
+          
+          onPress={()=> showAlert('Boton Morado Puchado')}
+        />
+      </View>
 
-      {/* Este estilo si va a funcionar ya que el componente "Texto" */}
-      <Texto style={styles.blue} ></Texto>
-      <Texto style={styles.red}></Texto>
-      <Texto style={styles.green}></Texto>
-      <Boton/>
+      <View style={styles.section}> 
+         <Text style={styles.description}>Dos Botones:</Text>
+         <View style={styles.buttonRow}>
+          <Button
+            title='Izquierda'
+            onPress={()=> showAlert('Boton Izquierda')}
+          />
+         <View style={styles.buttonSpacer}/>
+          <Button
+            title='Derecha'
+            onPress={()=> showAlert('Boton Derecha')}
+          />
+         </View>
+      </View> 
     </View>
+    
   );
 }
-// Zona de estilos
-// Estilos para la vista principal de la aplicacion
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'stretch', //para alinear los elementos al centro
-    flexDirection: 'colum', //disposicion de los elementos
-    //justify sirve para alinear los elementos
+    padding: 20,
+    backgroundColor: 'white',
     justifyContent: 'center',
-    justifyContent: 'space-around' , //para alinear los elementos 
   },
-
-  //aca creamos la clase texto
-  //para darle estilos a los textos
-  text:{
-    //para el color del texto
-    color: 'purple',
-    //para el tamaño del texto
-    fontSize: 28,
+  title:{
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 30,
+    color: '#800080', 
   },
-  //aca creamos estilos para textos de colores individuales
-  //disposicion flex
-  
-  blue:{backgroundColor: 'blue',  },
-  red:{backgroundColor: 'red',},
-  green:{backgroundColor: 'green',},
-
+  section:{
+    marginBottom: 20,
+  },
+  description:{
+    fontSize: 16,
+    marginBottom: 10,
+    color: '#333333',
+  },
+  buttonRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+    buttonSpacer: {
+    width: 10,
+  }
 });
