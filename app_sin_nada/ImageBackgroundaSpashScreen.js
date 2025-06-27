fe8e3b8e207e8bba97f6dcb57acb246f09adaa89
XxR1ch4rdxX <167234180+XxR1ch4rdxX@users.noreply.github.com>
2025-06-27 14:37:04 -0600

    SplashScreen
    Se agegaron cambios a ImageBackoung Y SpashScreen para completar la parte de la practica que faltaba.

--- app_sin_nada/ImageBackgroundaSpashScreen.js
+++ app_sin_nada/ImageBackgroundaSpashScreen.js
@@ -2,6 +2,22 @@ import React,{useEffect,useState} from "react";
 import {View,StyleSheet, Text, ImageBackground, ActivityIndicator} from "react-native";
 
 export default function App(){
+   //spashScreen
+   const[loading, setLoading] = useState(true);
+    useEffect(()=> {
+        setTimeout(()=> setLoading(false), 5000);
+    }, [])
+
+    if (loading) {
+        return(
+            <View style={styles.splash}>
+                <Text style={styles.splashtxt} >Cargando...</Text>
+                <ActivityIndicator size="large" color="white"/>
+            </View>    
+        );
+    }
+
+
    return(
      <ImageBackground
     source={{uri: 'https://images.unsplash.com/photo-1506744038136-46273834b3fb'}}
@@ -30,5 +46,24 @@ const styles = StyleSheet.create({
     text:{
         color: 'white',
         fontSize: 24,
-    }
+    },
+    splash:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgb(0, 0, 0)',
+    },
+    splashtxt:{
+        color: "rgb(255, 255, 255)",
+        fontSize: 28,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    title: {
+        color: 'white',
+        fontSize: 32,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    
 });
---
+++ app_sin_nada/Practica11.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet,
+    Text,
+    View,
+    Button,
+    ActivityIndicator,
+    ImageBackground,
+    Switch,TextInput,
+    Alert,
+    SafeAreaView,
+    Platform} from 'react-native';
+
