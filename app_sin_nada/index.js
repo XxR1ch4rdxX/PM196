900c9ee1aaa59e1715bdf51c32a8128acb68ee42
XxR1ch4rdxX <167234180+XxR1ch4rdxX@users.noreply.github.com>
2025-06-30 18:46:11 -0600

    ScrollView

--- app_sin_nada/index.js
+++ app_sin_nada/index.js
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo';
 
-import App from './Practica11';
+import App from './ScrollView';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
